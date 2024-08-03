df906ee80b84013bd883f6ab9dd6ed7f4bcb54b6
ShinSub98 <wjdtjq26@naver.com>
2024-08-03 22:32:35 +0900

    <fix>:fix issues

--- src/main/java/com/slide_todo/slide_todoApp/dto/jwt/RefreshTokenDTO.java
+++ src/main/java/com/slide_todo/slide_todoApp/dto/jwt/RefreshTokenDTO.java
@@ -1,5 +1,6 @@
 package com.slide_todo.slide_todoApp.dto.jwt;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ public class RefreshTokenDTO {
   @JsonProperty("refresh_token")
   private String refreshToken;
 
+  @JsonCreator
   public RefreshTokenDTO(String refreshToken) {
     this.refreshToken = refreshToken;
   }
