e0ae93ea345587ee292e5d52b988d294abe6a969
Jaeyoung You <jaeyoung0703@naver.com>
2024-08-05 03:05:19 +0900

    [REFACT] 코드리뷰 반영 : JOIN 메소드 수정

--- src/main/java/com/slide_todo/slide_todoApp/repository/group/GroupRepository.java
+++ src/main/java/com/slide_todo/slide_todoApp/repository/group/GroupRepository.java
@@ -2,7 +2,7 @@ package com.slide_todo.slide_todoApp.repository.group;
 
 import com.slide_todo.slide_todoApp.domain.goal.GroupGoal;
 import com.slide_todo.slide_todoApp.domain.group.Group;
-import com.slide_todo.slide_todoApp.domain.group.GroupMember;
+//import com.slide_todo.slide_todoApp.domain.group.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,10 +15,10 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 
     Optional<Group>  findBySecretCode(String secretCode);
 
-    @Query("SELECT gm FROM GroupMember gm LEFT JOIN FETCH gm.group WHERE gm.group.id = :groupId")
-    List<GroupMember> findByGroupIdWithGroupMember(@Param("groupId") Long groupId);
+    @Query("SELECT g FROM Group g LEFT JOIN FETCH g.members WHERE g.id = :groupId")
+    Group findGroupWithMembers(@Param("groupId") Long groupId);
 
-    @Query("SELECT gg FROM GroupGoal gg LEFT JOIN FETCH  gg.group WHERE gg.group.id = :groupId")
-    List<GroupGoal> findByGroupIdWithGroupGoal(@Param("groupId") Long groupId);
+    @Query("SELECT g FROM Group g LEFT JOIN FETCH g.groupGoals WHERE g.id = :groupId")
+    Group findGroupWithGoals(@Param("groupId") Long groupId);
 
 }
