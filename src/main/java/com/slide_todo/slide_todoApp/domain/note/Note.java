af076d4c22491016cdf907ca261976ccd75576bc
ShinSub98 <wjdtjq26@naver.com>
2024-08-04 16:45:13 +0900

    <feat>:add note entity

---
+++ src/main/java/com/slide_todo/slide_todoApp/domain/note/Note.java
@@ -0,0 +1,71 @@
+package com.slide_todo.slide_todoApp.domain.note;
+
+import com.slide_todo.slide_todoApp.domain.todo.Todo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Note {
+
+  @Id
+  @Column(name = "note_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne
+  @JoinColumn(name = "todo_id")
+  private Todo todo;
+
+  private String title;
+  @Lob
+  private String content;
+  private Boolean isDeleted;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+  @Builder
+  public Note(Todo todo, String title, String content) {
+    this.title = title;
+    this.content = content;
+    this.isDeleted = false;
+
+    /*연관 관계 편의 메소드*/
+    this.todo = todo;
+    this.todo.writeNote(this);
+  }
+
+  public void updateNote(String title, String content) {
+    if (title != null) {
+      this.title = title;
+    }
+    if (content != null) {
+      this.content = content;
+    }
+  }
+
+  public void deleteNote() {
+    this.isDeleted = true;
+  }
+}
--- src/main/java/com/slide_todo/slide_todoApp/domain/todo/Todo.java
+++ src/main/java/com/slide_todo/slide_todoApp/domain/todo/Todo.java
@@ -1,21 +1,25 @@
 package com.slide_todo.slide_todoApp.domain.todo;
 
 import com.slide_todo.slide_todoApp.domain.group.GroupMember;
+import com.slide_todo.slide_todoApp.domain.note.Note;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -35,8 +39,13 @@ public abstract class Todo {
   private String linkUrl;
   private Boolean isDeleted;
 
+  @OneToOne(mappedBy = "todo", fetch = FetchType.LAZY)
+  private Note note;
+
   @CreatedDate
   private LocalDateTime createdAt;
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
 
   public Todo(String title, String linkUrl) {
     this.title = title;
@@ -44,24 +53,42 @@ public abstract class Todo {
   }
 
   /**
-   * 할 일 삭제 처리
+   * 노트를 생성
+   *
+   * @param note
+   */
+  public void writeNote(Note note) {
+    this.note = note;
+  }
+
+  /**
+   * 할 일 삭제 처리<br> 노트가 등록 되어 있을 시 노트도 삭제합니다.
    */
   public void deleteTodo() {
     this.isDeleted = true;
+    if (this.note != null) {
+      this.note.deleteNote();
+    }
   }
 
   /**
    * 할 일의 제목과 링크를 수정.
+   *
    * @param title
    * @param linkUrl
    */
   public void updateTodo(String title, String linkUrl) {
-    if (title != null) {this.title = title;}
-    if (linkUrl != null) {this.linkUrl = linkUrl;}
+    if (title != null) {
+      this.title = title;
+    }
+    if (linkUrl != null) {
+      this.linkUrl = linkUrl;
+    }
   }
 
   /**
    * 할 일을 완료 처리
+   *
    * @return
    */
   public abstract Boolean checkDone();
