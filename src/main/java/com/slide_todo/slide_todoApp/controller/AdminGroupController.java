1c5c2728e6ef7cba0924a855960c1aa64a75924f
Jaeyoung You <jaeyoung0703@naver.com>
2024-08-11 21:26:15 +0900

    [fix] admin 계정 확인 추가

--- src/main/java/com/slide_todo/slide_todoApp/controller/AdminGroupController.java
+++ src/main/java/com/slide_todo/slide_todoApp/controller/AdminGroupController.java
@@ -3,10 +3,12 @@ package com.slide_todo.slide_todoApp.controller;
 import com.slide_todo.slide_todoApp.dto.group.GroupInfoDTO;
 import com.slide_todo.slide_todoApp.dto.group.admin.*;
 import com.slide_todo.slide_todoApp.service.group.AdminGroupService;
+import com.slide_todo.slide_todoApp.util.jwt.JwtProvider;
 import com.slide_todo.slide_todoApp.util.response.ResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
@@ -18,31 +20,39 @@ import org.springframework.web.bind.annotation.*;
 public class AdminGroupController {
 
     private final AdminGroupService adminGroupService;
+    private final JwtProvider jwtProvider;
 
     @GetMapping("")
     @Operation(summary = "그룹 관리 - 그룹 조회")
     public ResponseDTO<GroupListDTO> getAllGroups(
+            HttpServletRequest request,
             @Parameter(description = "그룹장 이름") @RequestParam(required = false) String nickname,
             @Parameter(description = "그룹 이름")@RequestParam(required = false) String group,
             @RequestParam int page, @RequestParam int limit){
+        Long adminId = jwtProvider.getAdminMemberId(request);
         return adminGroupService.getAllGroups(nickname,group,page,limit);
     }
 
     @DeleteMapping("")
     @Operation(summary = "그룹 관리 - 그룹 삭제")
-    public ResponseDTO<?> deleteGroup(@RequestBody GroupIdDTO groupIdDTO){
+    public ResponseDTO<?> deleteGroup(@RequestBody GroupIdDTO groupIdDTO,HttpServletRequest request){
+        Long adminId = jwtProvider.getAdminMemberId(request);
         return adminGroupService.deleteGroups(groupIdDTO);
     }
 
     @GetMapping("/{groupId}")
     @Operation(summary = "그룹 관리 - 그룹 상세 조회")
-    public ResponseDTO<GroupInfoListDTO> getGroupInfo(@PathVariable Long groupId){
+    public ResponseDTO<GroupInfoListDTO> getGroupInfo(@PathVariable Long groupId,HttpServletRequest request){
+        Long adminId = jwtProvider.getAdminMemberId(request);
         return adminGroupService.getGroupInfo(groupId);
     }
 
     @PatchMapping("/{groupId}")
     @Operation(summary = "그룹 관리 - 제목 or 초대코드 수정")
-    public ResponseDTO<GroupInfoListDTO> updateGroupInfo(@PathVariable Long groupId,@RequestBody GroupUpdateDTO groupUpdateDTO){
+    public ResponseDTO<GroupInfoListDTO> updateGroupInfo(
+            HttpServletRequest request,
+            @PathVariable Long groupId,@RequestBody GroupUpdateDTO groupUpdateDTO){
+        Long adminId = jwtProvider.getAdminMemberId(request);
         String title = groupUpdateDTO.getTitle();
         String secretCode = groupUpdateDTO.getSecretCode();
         return adminGroupService.updateGroupInfo(groupId,title,secretCode);
@@ -50,13 +60,19 @@ public class AdminGroupController {
 
     @DeleteMapping("/{groupId}/members/{memberId}")
     @Operation(summary = "그룹 관리 - 멤버 탈퇴")
-    public ResponseDTO<?> deleteMember(@PathVariable Long groupId,@PathVariable Long memberId){
+    public ResponseDTO<?> deleteMember(
+            HttpServletRequest request,
+            @PathVariable Long groupId,@PathVariable Long memberId){
+        Long adminId = jwtProvider.getAdminMemberId(request);
         return adminGroupService.deleteMemeber(groupId,memberId);
     }
 
     @PatchMapping("/{groupId}/leader/{memberId}")
     @Operation(summary = "그룹 관리 - 방장 변경")
-    public ResponseDTO<?> changeLeader(@PathVariable Long groupId,@PathVariable Long memberId){
+    public ResponseDTO<?> changeLeader(
+            HttpServletRequest request,
+            @PathVariable Long groupId,@PathVariable Long memberId){
+        Long adminId = jwtProvider.getAdminMemberId(request);
         return adminGroupService.changeLeader(groupId,memberId);
     }
 
