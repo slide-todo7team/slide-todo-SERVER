c59eb9b5beedba021246d5f28736e1444769429a
ShinSub98 <wjdtjq26@naver.com>
2024-08-16 14:09:57 +0900

    <fix>: Fixed error that memberInCharge field was always null

--- src/main/java/com/slide_todo/slide_todoApp/repository/group/BaseGroupMemberRepository.java
+++ src/main/java/com/slide_todo/slide_todoApp/repository/group/BaseGroupMemberRepository.java
@@ -1,8 +1,13 @@
 package com.slide_todo.slide_todoApp.repository.group;
 
 import com.slide_todo.slide_todoApp.domain.group.GroupMember;
+import java.util.List;
+import java.util.Map;
 
 public interface BaseGroupMemberRepository {
 
   GroupMember findByMemberIdAndGroupId(Long memberId, Long groupId);
+
+  /*그룹 할 일의 ID 목록으로 담당자 조회*/
+  Map<Long, GroupMember> findAllByGroupTodoIds(List<Long> groupTodoIds);
 }
